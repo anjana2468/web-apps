ea9d30096ac175556f61a98f21e3d330c4a72651
anjana2468 <anjana.shaju@dci-student.org>
2024-08-28 14:27:04 +0200

    added settings file

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=y_#qyg9u!3!dg@nant5m_c=_^_d6u%40fg&_+$zh7pe*#bkn(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["52.40.111.207"]
 
 
 # Application definition
